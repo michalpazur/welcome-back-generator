d6fd2b6784a91fd31e54a57c6aac413aa6721b56
michalpazur <michal.pazur@xentivo.pl>
2023-12-26 12:58:50 +0100

    Deleted: random article endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import dotenv from "dotenv";
 import express from "express";
 import { adminRouter } from "./routes/admin";
-import { randomRouter } from "./routes/random";
 import { sequelize } from "./util/sequelize";
 
 const app = express();
 dotenv.config();
 
 app.use("/admin", adminRouter);
-app.use("/random", randomRouter);
 
 const port = process.env.PORT || "3000";
 app.listen(port, () => {
