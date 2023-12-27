d10d2abc43de8ff3641e0a03aec6dbfef4b4ce7d
michalpazur <michal.pazur@xentivo.pl>
2023-12-27 02:36:37 +0100

    Added: generate picture endpoint

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
-FROM node:18-alpine
+FROM node:18
 WORKDIR /app
 COPY . .
+
+RUN apt-get update
+RUN apt-get install -y build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
+
 RUN yarn install
 RUN yarn build
 CMD [ "npm", "start" ]
--- src/routes/generate.ts
+++ src/routes/generate.ts
@@ -3,6 +3,12 @@ import express, { Request, Response } from "express";
 import { Op } from "sequelize";
 import { Entry } from "../models/entry";
 import { sequelize } from "../util/sequelize";
+import {
+  CanvasRenderingContext2D,
+  Image,
+  createCanvas,
+  loadImage,
+} from "canvas";
 
 type GenerateResponse = {
   died: string;
@@ -24,10 +30,11 @@ const findEntries = async () => {
   });
 
   while (!born) {
-    const randomIndex = Math.floor(Math.random() * allDead.length);
+    let randomIndex = Math.floor(Math.random() * allDead.length);
     died = allDead[randomIndex] as Entry;
     const year = died.endDate?.getFullYear();
-    born = await Entry.findOne({
+
+    const allBorn = await Entry.findAll({
       where: {
         [Op.and]: [
           sequelize.where(
@@ -38,6 +45,8 @@ const findEntries = async () => {
         ],
       },
     });
+    randomIndex = Math.floor(Math.random() * allBorn.length);
+    born = allBorn[randomIndex];
   }
 
   died = died as Entry;
@@ -57,7 +66,120 @@ const generateJson = async (req: Request, res: Response<GenerateResponse>) => {
   });
 };
 
+const width = 1200;
+const height = 1000;
+
+const textFieldHeight = 120;
+const padding = 20;
+const topTextWidth = width / 2 - padding * 2;
+
+const imageHeight = height - textFieldHeight * 2;
+const imageWidth = width / 2;
+
+const pasteImage = (img: Image, ctx: CanvasRenderingContext2D, x: number) => {
+  if (img.width / img.height > 3 / 2) {
+    const ratio = imageWidth / img.width;
+    const targetHeight = img.height * ratio;
+    const dy = (imageHeight - targetHeight) / 2;
+    ctx.drawImage(img, x, textFieldHeight + dy, imageWidth, targetHeight);
+    return;
+  }
+
+  if (img.width / img.height < 3 / 5) {
+    const ratio = imageHeight / img.height;
+    const targetWidth = img.width * ratio;
+    const dx = (imageWidth - targetWidth) / 2;
+    ctx.drawImage(img, x + dx, textFieldHeight, targetWidth, imageHeight);
+    return;
+  }
+
+  if (imageWidth / imageHeight < img.width / img.height) {
+    const srcWidth = (img.height * imageWidth) / imageHeight;
+    ctx.drawImage(
+      img,
+      (img.width - srcWidth) / 2,
+      0,
+      srcWidth,
+      img.height,
+      x,
+      textFieldHeight,
+      imageWidth,
+      imageHeight
+    );
+  } else {
+    const srcHeight = (img.width * imageHeight) / imageWidth;
+    ctx.drawImage(
+      img,
+      0,
+      (img.height - srcHeight) / 2,
+      img.width,
+      srcHeight,
+      x,
+      textFieldHeight,
+      imageWidth,
+      imageHeight
+    );
+  }
+};
+
+const generate = async (req: Request, res: Response) => {
+  res.setHeader("Content-Type", "image/png");
+  const { died, born } = await findEntries();
+
+  const canvas = createCanvas(width, height);
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#757575";
+  ctx.fillRect(0, 0, width, height);
+  ctx.font = "bold 72px Arial";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillStyle = "#FFFFFF";
+  ctx.strokeStyle = "#FFFFFF";
+
+  const endYear = died.endDate?.getFullYear() as number;
+
+  ctx.fillText(
+    `Died in ${endYear}`,
+    width / 4,
+    textFieldHeight / 2,
+    topTextWidth
+  );
+
+  const bottomTextY = height - textFieldHeight / 2;
+  ctx.fillText(
+    `Welcome back ${died.name}`,
+    width / 2,
+    bottomTextY,
+    width - padding * 2
+  );
+
+  ctx.font = "bold 55px Arial";
+  ctx.fillText(born.name, (3 * width) / 4, textFieldHeight / 4, topTextWidth);
+  ctx.fillText(
+    `Born in ${born.startDate.getFullYear()}`,
+    (3 * width) / 4,
+    (3 * textFieldHeight) / 4,
+    topTextWidth
+  );
+
+  try {
+    const diedImg = await loadImage(died.imgUrl);
+    const bornImg = await loadImage(born.imgUrl);
+    pasteImage(diedImg, ctx, 0);
+    pasteImage(bornImg, ctx, width / 2);
+  } catch (e: any) {}
+
+  canvas.toBuffer(
+    (err, buf) => {
+      res.send(buf);
+    },
+    "image/png",
+    { compressionLevel: 7 }
+  );
+};
+
 const router = express.Router();
 router.get("/json", generateJson);
+router.get("/", generate);
 
 export { router as generateRouter };
